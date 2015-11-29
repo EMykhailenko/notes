8e747c5052d4f133c66cb65eb52a48130af831ec
EMykhailenko <mihaylenko.evgeniy@gmail.com>
2015-11-29 23:19:54 +0200

    First step

---
+++ notes.js
@@ -0,0 +1,9 @@
+angular.module("notesApp", [])
+	.controller("HelloController", function($scope) {
+		$scope.greeting = "";
+		$scope.update = function() {
+			if ($scope.name) {
+				$scope.greeting = "Hello, "+$scope.name+"!";
+			}
+		}
+	});
